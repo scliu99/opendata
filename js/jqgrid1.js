3f22041830abc024459f469bfc4b85fa1da15d3e
scliu99 <scliu99@ntub.edu.tw>
2015-12-11 16:51:04 +0000

    add google map

--- js/jqgrid1.js
+++ js/jqgrid1.js
@@ -1,19 +1,89 @@
+var maxPage = 1;
+var map;
 $(document).ready(function () {
-    $("#jqGrid").jqGrid({
-        url: 'http://trirand.com/blog/phpjqgrid/examples/jsonp/getjsonp.php?callback=?&qwery=longorders',
-        mtype: "GET",
-        datatype: "jsonp",
-        colModel: [
-            { label: 'OrderID', name: 'OrderID', key: true, width: 75 },
-            { label: 'Customer ID', name: 'CustomerID', width: 150 },
-            { label: 'Order Date', name: 'OrderDate', width: 150 },
-            { label: 'Freight', name: 'Freight', width: 150 },
-            { label:'Ship Name', name: 'ShipName', width: 150 }
-        ],
-		viewrecords: true,
-        width: 780,
-        height: 250,
-        rowNum: 20,
-        pager: "#jqGridPager"
+    var page = 1;
+    var rows = 20;
+    
+    fetchData(page, rows);
+    $("#pageRow").change(function(){
+        page = 1; 
+        rows = $(this).val();
+        $("#pageNo").val(page);
+        //console.dir($(this).val());
+        fetchData(page, rows);
     });
+    $("#prePage").click(function(){
+        if(page <= 1) {
+            page = 1;
+        }else{
+            page--;
+            fetchData(page, rows);        
+        }
+        $("#pageNo").val(page);
+    });
+    $("#nextPage").click(function(){
+        if(page >= maxPage) {
+            page = maxPage;
+        }else{
+            page++;
+            fetchData(page, rows);        
+        }
+        $("#pageNo").val(page);
+    });
+    $("#pageNo").blur(function(){
+        page = $(this).val();
+        if(page <= 1){
+            page = 1;
+        }else if(page >= maxPage){
+            page = maxPage;
+        }
+        fetchData(page, rows); 
+        $("#pageNo").val(page);
+    });
+   
 });
+
+
+
+function fetchData(page, rows){
+    $("#message").show();
+    $.get("service/wifiod.php?page="+page+"&rows="+rows, function(data){
+        maxPage = data.total;
+        //$("myGrid").app
+        //for(var item in data)
+        var $myGrid = $("#myGrid");
+        $myGrid.empty();
+        var head = "<th><td>ID</td><td>地區</td><td>地址</td><td>熱點類別</td></th>";
+        $myGrid.append(head);
+        for(var i=0; i<data.rows.length; i++){
+            //console.dir(data.rows[i]);
+            var row = "<tr>";
+            row += "<td>"+ data.rows[i]._id  +"</td>";
+            row += "<td>"+ data.rows[i].HOTSPOT_NAME  +"</td>";
+            row += "<td>"+ data.rows[i].AREA  +"</td>";
+            row += "<td>"+ data.rows[i].ADDRESS  +"</td>";
+            row += "<td>"+ data.rows[i].HOTSPOT_TYPE  +"</td>";
+            row += "</tr>";
+            $myGrid.append(row);
+            var lat = data.rows[i].LAT - 0;
+            var lng = data.rows[i].LNG - 0;
+            var marker = new google.maps.Marker({
+                position: {lat: lat, lng: lng},
+                map: map,
+                title: 'Hello World!'
+            });
+        }
+        $("#message").hide();    
+    },"json")
+    
+    
+    
+    
+}
+ 
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 25.02211, lng: 121.4118},
+        zoom: 13
+    }); 
+}
--- js/jqgrid2.js
+++ js/jqgrid2.js
@@ -1,8 +1,19 @@
 $(document).ready(function () {
-    
-    $.get("service/wifiod.php?limit=10", function(data){
-        console.dir(data.result.results[0]);
-    },"json")
-    
-    
+    $("#jqGrid").jqGrid({
+        url: 'http://trirand.com/blog/phpjqgrid/examples/jsonp/getjsonp.php?callback=?&qwery=longorders',
+        mtype: "GET",
+        datatype: "jsonp",
+        colModel: [
+            { label: 'OrderID', name: 'OrderID', key: true, width: 75 },
+            { label: 'Customer ID', name: 'CustomerID', width: 150 },
+            { label: 'Order Date', name: 'OrderDate', width: 150 },
+            { label: 'Freight', name: 'Freight', width: 150 },
+            { label:'Ship Name', name: 'ShipName', width: 150 }
+        ],
+		viewrecords: true,
+        width: 780,
+        height: 250,
+        rowNum: 20,
+        pager: "#jqGridPager"
+    });
 });
